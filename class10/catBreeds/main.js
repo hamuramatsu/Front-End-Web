f05cbf94debb12029c6842f496e48bfa9aadc462
hamuramatsu <151678468+hamuramatsu@users.noreply.github.com>
2024-11-07 20:19:14 -0500

    cats breeds

---
+++ class10/catBreeds/main.js
@@ -0,0 +1,23 @@
+fetch("cats.json").then(response => {
+    return response.json();
+  }).then(data => {
+    console.log(data);
+    mainContent.innerHTML = `<h2>${data.description}</h2>`;
+    let selectElement = document.createElement('select');
+  
+    mainContent.append(selectElement);
+    for(let breed of data.cats){
+      let breedElement = document.createElement('option');
+      breedElement.innerHTML = breed;
+      selectElement.append(breedElement);
+    }
+  
+    selectElement.onchange = () => {
+      wikipediaFrame.setAttribute("src", "https://en.wikipedia.org/wiki/" + selectElement.value + " cat");
+    };
+  
+  }).catch (error => {
+      consol.error(error);
+  });
+  
+  document.body.style.backgroundColor = "lime";
