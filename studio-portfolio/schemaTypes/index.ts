63c1292f1f50469302c8e69a53e74c9f24b1b235
hamuramatsu <151678468+hamuramatsu@users.noreply.github.com>
2024-12-03 22:54:15 -0500

    types

--- studio-portfolio/schemaTypes/index.ts
+++ studio-portfolio/schemaTypes/index.ts
@@ -1,3 +1,4 @@
 import {postType} from './postType'
+import {workType} from './workType'
 
-export const schemaTypes = [postType]
+export const schemaTypes = [postType, workType];
---
+++ studio-portfolio/schemaTypes/workType.ts
@@ -0,0 +1,55 @@
+import {defineField, defineType} from 'sanity'
+
+export const workType = defineType({
+    name: 'work',
+    title: 'Work',
+    type: 'document',
+    fields: [
+      defineField({
+        name: 'title',
+        type: 'string',
+        validation: (rule) => rule.required(),
+      }),
+      defineField({
+        name: 'slug',
+        type: 'slug',
+        options: {source: 'title'},
+        validation: (rule) => rule.required(),
+      }),
+      defineField({
+        name: 'publishedAt',
+        type: 'datetime',
+        initialValue: () => new Date().toISOString(),
+        validation: (rule) => rule.required(),
+      }),
+      defineField({
+        name: 'yearCreated',
+        type: 'string',
+      }),
+      defineField({
+        name: 'medium',
+        type: 'string',
+      }),
+      defineField({
+        name: 'image1',
+        type: 'image',
+      }),
+      defineField({
+        name: 'image2',
+        type: 'image',
+      }),
+      defineField({
+        name: 'image3',
+        type: 'image',
+      }),
+      defineField({
+        name: 'image4',
+        type: 'image',
+      }),
+      defineField({
+        name: 'body',
+        type: 'array',
+        of: [{type: 'block'}],
+      }),
+    ],
+  })
